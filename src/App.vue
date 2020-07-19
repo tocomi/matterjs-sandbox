fa350ef9297595bc2458ce40966b64f35d1ad441
tocomi <tocomi@wish.ocn.ne.jp>
2020-07-19 17:14:23 +0900

    feat: use matter.js

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,111 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
-  </div>
+  <div id="app" />
 </template>
 
 <script lang="ts">
 import Vue from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
+import Matter from "matter-js";
+
+const {
+  Engine,
+  Render,
+  Runner,
+  World,
+  Bodies,
+  Mouse,
+  MouseConstraint
+} = Matter;
+
+const SIZES = [20, 32, 44];
+const STARTS = [60, 120, 180, 240, 300];
+
+const selectRandom = (array: number[]) => {
+  return array[Math.floor(Math.random() * array.length)];
+};
+
+const makeCircleRandom = () => {
+  const size = selectRandom(SIZES);
+  const start = selectRandom(STARTS);
+  const circle = Bodies.circle(start, 0, size, {
+    restitution: 0.8, // 弾力性
+    frictionAir: 0.03 // 空気抵抗
+  });
+  return circle;
+};
+
+const addCircles = (world: Matter.World) => {
+  let count = 0;
+  const max = 30;
+
+  const add = (world: Matter.World) => {
+    if (count === max) return;
+    setTimeout(() => {
+      World.add(world, makeCircleRandom());
+      count++;
+      add(world);
+    }, 500);
+  };
+
+  add(world);
+};
+
+const render = () => {
+  const engine = Engine.create();
+  const world = engine.world;
+
+  /**
+   * 描画エリアの設定
+   */
+  const render = Render.create({
+    element: document.body,
+    engine: engine,
+    options: {
+      width: 352,
+      height: 512,
+      background: "#eee",
+      wireframes: false
+    }
+  });
+
+  Render.run(render);
+
+  const runner = Runner.create();
+  Runner.run(runner, engine);
+
+  /**
+   * 床と左右の壁を作成
+   */
+  const floor = Bodies.rectangle(178, 512, 352, 8, {
+    isStatic: true
+  });
+
+  const wallLeft = Bodies.rectangle(0, 300, 8, 600, {
+    isStatic: true
+  });
+
+  const wallRight = Bodies.rectangle(352, 300, 8, 600, {
+    isStatic: true
+  });
+
+  World.add(world, [floor, wallLeft, wallRight]);
+
+  /**
+   * マウス操作の設定
+   */
+  const mouse = Mouse.create(render.canvas);
+  const mouseConstraint = MouseConstraint.create(engine, {
+    mouse: mouse
+  });
+
+  World.add(world, mouseConstraint);
+
+  addCircles(world);
+};
 
 export default Vue.extend({
   name: "App",
-  components: {
-    HelloWorld
+  created() {
+    render();
   }
 });
 </script>
